d8a06c37b91858e28119c2d38cb916235ec18822
Jim McVea <117947708+awsjim@users.noreply.github.com>
2023-10-24 11:29:58 -0400

    Update docusaurus.config.js

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Jim\'s Test Site',
-  tagline: 'Robots are cool',
+  tagline: 'Robots are awesome',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
